4c5bed4c2a485779f6636f2fad1859fefa5f647c
Rohallah Shayeste <rohallahshayeste@outlook.com>
2019-07-28 22:34:39 +0430

    Add project files.

---
+++ TagHealper/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.ExpressionTranslators.Internal;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TagHealper.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<string> users = new List<string>
+        {
+            "Ali",
+            "Reza",
+            "Rohallah",
+            "Meran",
+            "Zahra",
+            "Ehsan",
+            "Mahdi"
+        };
+            return View(users);
+        }
+    }
+}
---
+++ TagHealper/TagHeapers/ShayesteTagHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TagHealper.TagHeapers
+{
+    public class ShayesteTagHelper : TagHelper
+    {
+
+        public string UserName { get; set; }
+        public List<string> Users { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (Users.Contains(UserName))
+            {
+                output.TagName = "p";
+                output.Attributes.Add("class","alert alert-danger") ;
+            }
+            else
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+}
